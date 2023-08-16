31fb71806b76f457a996ce028f20dba20bc82007
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-08-17 01:14:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,25 @@ def get_driver():
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
+options.add_argument(f"--window-size=1290x550")
 
 driver = get_driver()
 driver.get("https://langchain-quickstart.streamlit.app/~/+/")
+
+time.sleep(10)
+    
+# Explicitly wait for an essential element to ensure content is loaded
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+
+# Get scroll height and width
+scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+
+# Set window size
+driver.set_window_size(scroll_width, scroll_height)
+
+# Now, capture the screenshot
+driver.save_screenshot('screenshot.png')
 
 st.code(driver.page_source)
 
