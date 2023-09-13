61f49ec074ae95e16751e6e1fe64ebb73035ceac
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-14 00:16:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if app_url:
     driver = get_driver()
     driver.get(f"{app_url}/~/+/")
     
-    time.sleep(10)
+    #time.sleep(10)
         
     # Explicitly wait for an essential element to ensure content is loaded
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
