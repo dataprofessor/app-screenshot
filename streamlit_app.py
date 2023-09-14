cadc962ec50382f97c159ec2bc804f1124831ecd
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-14 12:06:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,12 +73,14 @@ def add_corners(im, rad):
     circle = Image.new('L', (rad * 2, rad * 2), 0)
     draw = ImageDraw.Draw(circle)
     draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
+    
     alpha = Image.new('L', im.size, 255)
     w, h = im.size
+    
+    # Apply rounded corners only to the top
     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
-    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
     alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
-    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    
     im.putalpha(alpha)
     return im
 
