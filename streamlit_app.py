4f92dfc22d713e5f030b35f58db06d0311cf75b1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-14 09:23:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@
 
 import streamlit as st
 import time
+import psutil
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -22,6 +23,12 @@ from selenium.webdriver.chrome.service import Service
 st.set_page_config(page_title="🎈 App Screenshot")
 st.title('🎈 App Screenshot')
 st.warning('An app for taking screenshot of a Streamlit app.')
+
+# Getting % usage of virtual_memory ( 3rd field)
+st.caption(f'RAM memory % used: {psutil.virtual_memory()[2]}')
+# Getting usage of virtual_memory in GB ( 4th field)
+st.caption(f'RAM Used (GB): {psutil.virtual_memory()[3]/1000000000}')
+
 
 # Input URL
 app_url = st.text_input('App URL', 'https://langchain-quickstart.streamlit.app').rstrip('/')
