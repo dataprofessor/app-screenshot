c95b35656c69377e2741e968f246ba7606f4ef78
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-14 09:32:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,22 +24,6 @@ st.set_page_config(page_title="🎈 App Screenshot")
 st.title('🎈 App Screenshot')
 st.warning('An app for taking screenshot of a Streamlit app.')
 
-# Input URL
-app_url = st.text_input('App URL', 'https://langchain-quickstart.streamlit.app').rstrip('/')
-app_name = app_url.replace('https://','').replace('.streamlit.app','')
-st.divider()
-
-# Settings
-with st.sidebar:
-    st.header('⚙️ Settings')
-    width = st.slider('Width', 426, 3840, 1000)
-    height = st.slider('Height', 240, 2160, 540)
-
-    # Getting % usage of virtual_memory ( 3rd field)
-    st.caption(f'RAM used (%): {psutil.virtual_memory()[2]}')
-    # Getting usage of virtual_memory in GB ( 4th field)
-    st.caption(f'RAM used (GB): {round(psutil.virtual_memory()[3]/1000000000, 1)}')
-
 @st.cache_resource
 def get_driver():
     options = webdriver.ChromeOptions()
@@ -53,37 +37,63 @@ def get_driver():
     
     return webdriver.Chrome(service=service, options=options)
 
-if app_url:
-    driver = get_driver()
-    if app_url.endswith('streamlit.app'):
-        driver.get(f"{app_url}/~/+/")
-    else:
-        driver.get(app_url)
+# Settings
+with st.sidebar:
+    st.header('⚙️ Settings')
+    width = st.slider('Width', 426, 3840, 1000)
+    height = st.slider('Height', 240, 2160, 540)
+
+    # Getting % usage of virtual_memory ( 3rd field)
+    st.caption(f'RAM used (%): {psutil.virtual_memory()[2]}')
+    # Getting usage of virtual_memory in GB ( 4th field)
+    st.caption(f'RAM used (GB): {round(psutil.virtual_memory()[3]/1000000000, 1)}')
+
+
+# Input URL
+with st.form("my_form"):
+    app_url = st.text_input('App URL', 'https://langchain-quickstart.streamlit.app').rstrip('/')
+    app_name = app_url.replace('https://','').replace('.streamlit.app','')
+
+    st.divider()
     
-    time.sleep(2)
-        
-    # Explicitly wait for an essential element to ensure content is loaded
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
-    
-    # Get scroll height and width
-    #scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-    #scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-    
-    # Set window size
-    #driver.set_window_size(scroll_width, scroll_height)
-    
-    # Now, capture the screenshot
-    driver.save_screenshot('screenshot.png')
-    
-    with Image.open('screenshot.png') as image:
-        st.image(image)
-    
-    with open("screenshot.png", "rb") as file:
-        btn = st.download_button(
-                label="Download image",
-                data=file,
-                file_name=f"{app_name}.png",
-                mime="image/png"
-              )
-        if btn:
-            st.cache_resource.clear()
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+
+
+
+
+
+        if app_url:
+            driver = get_driver()
+            if app_url.endswith('streamlit.app'):
+                driver.get(f"{app_url}/~/+/")
+            else:
+                driver.get(app_url)
+            
+            time.sleep(2)
+                
+            # Explicitly wait for an essential element to ensure content is loaded
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            
+            # Get scroll height and width
+            #scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+            #scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+            
+            # Set window size
+            #driver.set_window_size(scroll_width, scroll_height)
+            
+            # Now, capture the screenshot
+            driver.save_screenshot('screenshot.png')
+            
+            with Image.open('screenshot.png') as image:
+                st.image(image)
+            
+            with open("screenshot.png", "rb") as file:
+                btn = st.download_button(
+                        label="Download image",
+                        data=file,
+                        file_name=f"{app_name}.png",
+                        mime="image/png"
+                      )
+                if btn:
+                    st.cache_resource.clear()
