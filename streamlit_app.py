6f1fcbf0c2c761d521361b7af7e52380994418d5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-14 19:38:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ if file_exists:
     draw = ImageDraw.Draw(img)
     
     # Define the coordinates of the rectangle (left, top, right, bottom)
-    rectangle_coordinates = [(0, 0), (w + 10, h + 10)]
+    rectangle_coordinates = [(0, 0), (w + 0, h + 0)]
     
     # Draw the white rectangle
     draw.rectangle(rectangle_coordinates, fill='white')
